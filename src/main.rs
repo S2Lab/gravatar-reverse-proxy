f74ec1490a4fd9f08fdf1161e857826ce1fc6192
星川かえで <futursolo@icloud.com>
2017-04-07 01:39:06 +0800

    Add LruCache for cache control(50% done).

--- src/main.rs
+++ src/main.rs
@@ -34,9 +34,13 @@ extern crate lazy_static;
 extern crate regex;
 extern crate hyper_rustls;
 extern crate urlencoded;
+extern crate persistent;
+extern crate lru_cache;
+extern crate time;
 
 use std::vec::Vec;
 use hyper::Client as HyperClient;
+use hyper::client::Response as HyperResponse;
 use hyper::net::HttpsConnector;
 use router::{Router, NoRoute};
 use iron::prelude::*;
@@ -48,10 +52,15 @@ use hyper_rustls::TlsClient as RustlsClient;
 use urlencoded::{UrlEncodedQuery, UrlDecodingError};
 use std::collections::HashMap;
 use std::option::Option;
-use iron::headers::ContentType;
+use iron::headers::{ContentType, ContentDisposition, CacheControl, EntityTag, HttpDate};
 use std::ops::Deref;
 use std::fmt;
 use std::error::Error;
+use lru_cache::LruCache;
+use persistent::Write;
+use iron::typemap::Key;
+use time::PreciseTime;
+use std::result::Result;
 
 
 fn verify_md5(md5: &str) -> bool {
@@ -81,16 +90,108 @@ impl Error for AvatarError {
     }
 }
 
+#[derive(Clone, Eq, PartialEq, Hash)]
+struct CacheControlKey {
+    email_md5: String,
+    size: Option<i64>,
+    default: Option<String>,
+}
+
+
+impl CacheControlKey {
+    fn from_request(req: &mut Request) -> Result<CacheControlKey, Response> {
+        let email_md5 = req.extensions
+            .get::<Router>()
+            .unwrap()
+            .find("email_md5")
+            .unwrap_or("")
+            .to_lowercase();
+
+        if !verify_md5(email_md5.as_str()) {
+            return Err(Response::with((status::NotFound)));
+        }
+
+        let maybe_query = req.get::<UrlEncodedQuery>()
+            .map(|val| Some(val))
+            .unwrap_or_else(|err: UrlDecodingError| {
+                match err {
+                    UrlDecodingError::EmptyQuery => Some(HashMap::new()),
+                    _ => None,
+                }
+            });
+
+        let query = match maybe_query {
+            Some(m) => m,
+            None => return Err(Response::with((status::BadRequest))),
+        };
+
+        let size_str = query.get("s")
+            .map(|s_vec| s_vec.clone())
+            .map(|s_vec| { if s_vec.len() > 0 { Some(s_vec) } else { None } })
+            .unwrap_or(None)
+            .map(|s_vec| s_vec[0].clone());
+
+        let default = query.get("d")
+            .map(|d_vec| d_vec.clone())
+            .map(|d_vec| { if d_vec.len() > 0 { Some(d_vec) } else { None } })
+            .unwrap_or(None)
+            .map(|d_vec| d_vec[0].clone());
+
+        let size: Option<i64> = match size_str {
+            Some(m) => {
+                match m.parse::<i64>() {
+                    Ok(m) => Some(m),
+                    Err(_) => return Err(Response::with((status::BadRequest))),
+                }
+            }
+            None => None,
+        };
+
+        Ok(CacheControlKey {
+            email_md5: email_md5,
+            size: size,
+            default: default,
+        })
+    }
+}
+
+
+struct CacheControlData {
+    pub create_time: PreciseTime,
+    pub max_age: u64,
+    pub etag: EntityTag,
+    pub last_modified: HttpDate,
+    pub content_type: ContentType,
+    pub content_length: u64,
+    pub content_disposition: ContentDisposition,
+    pub cache_control: CacheControl,
+}
+
+
+impl CacheControlData {
+    fn from_response(res: &HyperResponse) -> AvatarResult<CacheControlData> {
+        panic!();
+    }
+}
+
+
+#[derive(Copy, Clone)]
+struct Cache;
+impl Key for Cache {
+    type Value = LruCache<CacheControlKey, CacheControlData>;
+}
+
 
 struct Avatar {
     pub email_md5: String,
     pub size: Option<i64>,
     pub content_type: ContentType,
     pub buf: Vec<u8>,
+    pub cc_key: CacheControlKey, // pub cc_data: CacheControlData,
 }
 
 
-type AvatarResult = Result<Avatar, AvatarError>;
+type AvatarResult<T> = Result<T, AvatarError>;
 
 impl Avatar {
     fn make_origin_url(email_md5: &str, size: Option<i64>, default: Option<String>) -> String {
@@ -114,91 +215,66 @@ impl Avatar {
         origin_url
     }
 
-    pub fn fetch(email_md5: &str, size: Option<i64>, default: Option<String>) -> AvatarResult {
-
-        lazy_static! {
-            static ref HYPER_CLIENT: HyperClient = HyperClient::with_connector(HttpsConnector::new(RustlsClient::new()));
-        }
-
-        let origin_url = Avatar::make_origin_url(email_md5, size.clone(), default.clone());
-
-        let mut origin_res = try!(HYPER_CLIENT.get(origin_url.as_str())
-            .send()
-            .map_err(|_| AvatarError { status: None }));
-
-        if origin_res.status != hyper::Ok {
-            return Err(AvatarError { status: Some(origin_res.status) });
+    pub fn from_response(res: &mut HyperResponse, cc_key: CacheControlKey) -> AvatarResult<Avatar> {
+        if res.status != hyper::Ok {
+            return Err(AvatarError { status: Some(res.status) });
         }
 
         let mut avatar_body: Vec<u8> = Vec::new();
 
-        try!(origin_res.read_to_end(&mut avatar_body)
+        try!(res.read_to_end(&mut avatar_body)
             .map_err(|_| AvatarError { status: Some(status::BadGateway) }));
 
-        let content_type = try!(origin_res.headers
+        let content_type = try!(res.headers
                 .get::<ContentType>()
                 .ok_or(AvatarError { status: Some(status::BadGateway) }))
             .clone();
 
         Ok(Avatar {
-            email_md5: email_md5.to_string(),
-            size: size.clone(),
+            email_md5: cc_key.email_md5.clone(),
+            size: cc_key.size.clone(),
             content_type: content_type,
             buf: avatar_body,
+            cc_key: cc_key,
         })
+
+    }
+
+    pub fn fetch(cc_key: CacheControlKey) -> AvatarResult<Avatar> {
+        lazy_static! {
+            static ref HYPER_CLIENT: HyperClient = HyperClient::with_connector(HttpsConnector::new(RustlsClient::new()));
+        }
+
+        let origin_url = Avatar::make_origin_url(cc_key.email_md5.as_str(),
+                                                 cc_key.size.clone(),
+                                                 cc_key.default.clone());
+
+        let mut origin_res = try!(HYPER_CLIENT.get(origin_url.as_str())
+            .send()
+            .map_err(|_| AvatarError { status: None }));
+
+        Avatar::from_response(&mut origin_res, cc_key)
     }
 }
 
 
-fn handler(req: &mut Request) -> IronResult<Response> {
-    let email_md5 = req.extensions
-        .get::<Router>()
-        .unwrap()
-        .find("email_md5")
-        .unwrap_or("")
-        .to_lowercase();
+fn handler(mut req: &mut Request) -> IronResult<Response> {
+    let cache_mutex = req.get::<Write<Cache>>().unwrap();
 
-    if !verify_md5(email_md5.as_str()) {
-        return Ok(Response::with((status::NotFound)));
+    let cc_key = match CacheControlKey::from_request(&mut req) {
+        Ok(v) => v,
+        Err(res) => return Ok(res),
+    };
+
+    {
+        let mut cache = cache_mutex.lock().unwrap();
+
+        if cache.contains_key(&cc_key) {
+            panic!();
+        }
     }
 
-    let maybe_query = req.get::<UrlEncodedQuery>()
-        .map(|val| Some(val))
-        .unwrap_or_else(|err: UrlDecodingError| {
-            match err {
-                UrlDecodingError::EmptyQuery => Some(HashMap::new()),
-                _ => None,
-            }
-        });
-
-    let query = match maybe_query {
-        Some(m) => m,
-        None => return Ok(Response::with((status::BadRequest))),
-    };
-
-    let size_str = query.get("s")
-        .map(|s_vec| s_vec.clone())
-        .map(|s_vec| { if s_vec.len() > 0 { Some(s_vec) } else { None } })
-        .unwrap_or(None)
-        .map(|s_vec| s_vec[0].clone());
-
-    let default = query.get("d")
-        .map(|d_vec| d_vec.clone())
-        .map(|d_vec| { if d_vec.len() > 0 { Some(d_vec) } else { None } })
-        .unwrap_or(None)
-        .map(|d_vec| d_vec[0].clone());
-
-    let size: Option<i64> = match size_str {
-        Some(m) => {
-            match m.parse::<i64>() {
-                Ok(m) => Some(m),
-                Err(_) => return Ok(Response::with((status::BadRequest))),
-            }
-        }
-        None => None,
-    };
-
-    let avatar = match Avatar::fetch(email_md5.as_str(), size, default) {
+    let avatar = match Avatar::fetch(cc_key) {
         Ok(v) => v,
         Err(_) => return Ok(Response::with((status::BadGateway))),
     };
@@ -288,6 +364,8 @@ fn main() {
     let mut chain = Chain::new(router);
     chain.link_after(CustomErrorMsg);
     chain.link_after(XPoweredBy);
+
+    chain.link(Write::<Cache>::both(LruCache::new(102400)));
 
     Iron::new(chain).http("localhost:3000").unwrap();
 }
